bc3b135b955957984dd38a9be0ebc25d3b5d16f1
Soumen Mandal <110480120+Soumen4894@users.noreply.github.com>
2024-07-30 08:41:48 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "study-monk-server-ebon.vercel.app"
+const BASE_URL = "https://study-monk-server-ebon.vercel.app/"
 
 // AUTH ENDPOINTS
 export const endpoints = {
