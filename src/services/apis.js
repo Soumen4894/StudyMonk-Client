85d41c1e157ce315094d02beb0ba78b2db231e4c
Soumen4894 <soumenmandal0008@gmail.com>
2024-07-31 09:36:11 +0530

    sfsd

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,6 @@
 //const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "https://study-monk-server-ebon.vercel.app"
+const BASE_URL = "study-monk-server-phi.vercel.app"
+//const BASE_URL = "https://study-monk-server.onrender.com"
 
 // AUTH ENDPOINTS
 export const endpoints = {
