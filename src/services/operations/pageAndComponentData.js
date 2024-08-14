b16e3cf53605c6e80ff5842aeffb96ae0d97fb7c
Soumen4894 <soumenmandal0008@gmail.com>
2024-08-14 11:54:11 +0530

    changes

--- src/services/operations/pageAndComponentData.js
+++ src/services/operations/pageAndComponentData.js
@@ -7,8 +7,10 @@ export const getCatalogaPageData = async(categoryId) => {
   const toastId = toast.loading("Loading...");
   let result = [];
   try{
+      
         const response = await apiConnector("POST", catalogData.CATALOGPAGEDATA_API, 
         {categoryId: categoryId,});
+        
 
         if(!response?.data?.success)
             throw new Error("Could not Fetch Category page data");
